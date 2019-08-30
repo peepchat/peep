95e8eca2d3be41cb9e468e2282eb70051bf876e1
John Jeong <jjohnvi@Johns-MacBook-Pro.local>
2019-08-30 12:12:41 -0500

    login works

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
--- src/redux/AuthReducer/AuthReducer.js
+++ src/redux/AuthReducer/AuthReducer.js
@@ -3,11 +3,74 @@ import axios from "axios";
 const initialState = {
   email: "",
   password: "",
-  nickname: ""
+  nickname: "",
+  loading: false,
+  user: {}
 };
 
 const UPDATE_STATE = "UPDATE_STATE";
-const RESET_FIELD = "RESET_FIELDS";
+const RESET_FIELDS = "RESET_FIELDS";
 const LOGIN_USER = "LOGIN_USER";
 const CHECK_USER_LOGGED_IN = "CHECK_USER_LOGGED_IN";
 const LOGOUT_USER = "LOGOUT_USER";
+
+export const updateState = event => {
+  return {
+    type: UPDATE_STATE,
+    payload: event
+  };
+};
+
+export const checkUserLoggedIn = () => {
+  return {
+    type: CHECK_USER_LOGGED_IN,
+    payload: axios.get("/auth/user")
+  };
+};
+
+export const loginUser = (email, password) => {
+  return {
+    type: LOGIN_USER,
+    payload: axios.post("/auth/login", {
+      email: email,
+      password: password
+    })
+  };
+};
+
+export const logoutUser = () => {
+  return {
+    type: LOGOUT_USER,
+    payload: axios.get("/auth/logout")
+  };
+};
+
+export const resetFields = () => {
+  return {
+    type: RESET_FIELDS
+  };
+};
+
+export function authReducer(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case UPDATE_STATE:
+      return { ...state, ...payload };
+    case RESET_FIELDS:
+      return { ...state, ...payload };
+    case `${LOGIN_USER}_PENDING`:
+      return { ...state, loading: true };
+    case `${LOGIN_USER}_FULFILLED`:
+      return { ...state, loading: false, user: payload.data };
+    case `${CHECK_USER_LOGGED_IN}_PENDING`:
+      return { ...state, loading: true };
+    case `${CHECK_USER_LOGGED_IN}_FULFILLED`:
+      return { ...state, loading: false, user: payload.data };
+    case `${LOGOUT_USER}_PENDING`:
+      return { ...state, loading: true };
+    case `${LOGIN_USER}_FULFILLED`:
+      return { ...state, loading: false, user: {} };
+    default:
+      return state;
+  }
+}
