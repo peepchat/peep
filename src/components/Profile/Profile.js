2b52931186126188ef97527e5eb683fa3e3af1b6
Ali_Ahmadiavin <ahmadiavin@gmail.com>
2019-09-02 16:44:59 -0500

    profile page css/state changes\

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,19 +6,25 @@ import { connect } from "react-redux";
 const ProfileWrapper = styled.div`
   width: 100%;
   height: 100%;
-  left: 25%;
+  left: 5%;
   position: fixed;
   background: none;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  align-content: center;
   border: 1px solid lightgrey;
 `;
 
 const ChatBox = styled.div`
   width: 100%;
   height: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
 `;
 
 const MessageDiv = styled.div`
@@ -34,6 +40,47 @@ const WidgetButton = styled.button`
   border-radius: 50%;
 `;
 
+const ProfileBox = styled.div`
+  height: 60vh;
+  width: 50vw;
+  border-radius: 10px;
+  box-sizing: border-box;
+  background: rgba(32, 34, 37, 0.6);
+  border-color: #202225;
+  padding: 20px;
+  position: relative;
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 5px;
+`;
+const PropfileCard = styled.div`
+background: #202225
+border-width: 1px;
+    border-style: solid;
+    border-radius: 5px;
+`;
+const ProfileH2 = styled.h2`
+
+box-sizing: border-box;
+text-transform: uppercase;
+line-height: 20px;
+margin-bottom: 20px;
+margin-left: 10px;
+font-weight: 600;
+}
+`;
+const ProfileH3 = styled.h3`
+
+box-sizing: border-box;
+text-transform: uppercase;
+line-height: 20px;
+margin-bottom: 20px;
+margin-left: 10px;
+font-weight: 600;
+color: #b9bbbe;
+}
+`;
+
 const Profile = props => {
   const checkUploadResult = async (error, resultEvent) => {
     if (resultEvent.event === "success") {
@@ -43,7 +90,7 @@ const Profile = props => {
   };
 
   const [imageurl, setImageurl] = useState("");
-
+  const [dropdown, setDropdown] = useState(false);
   const widget = window.cloudinary.createUploadWidget(
     {
       cloudName: "dtkvcgoz6",
@@ -58,15 +105,39 @@ const Profile = props => {
   return (
     <ProfileWrapper>
       <ChatBox>
-        <WidgetButton onClick={() => widget.open()}>Cloudinary</WidgetButton>
+        <ProfileH2>My Account</ProfileH2>
+        <ProfileBox>
+          <PropfileCard>
+            <ProfileH3>Nickname: {props.nickname}</ProfileH3>
+            <ProfileH3>Email: {props.email}</ProfileH3>
+          </PropfileCard>
+          <WidgetButton onClick={() => widget.open()}>Cloudinary</WidgetButton>
+          <button onClick={() => setDropdown( !dropdown )}>
+            Edit
+          </button>
+          {dropdown === true ? (
+            <div>
+              stuff
+              <input />
+              <button>Edit</button>
+            </div>
+          ) : null}
+        </ProfileBox>
       </ChatBox>
 
-      <MessageDiv>MessageDiv</MessageDiv>
+      {/* <MessageDiv>MessageDiv</MessageDiv> */}
     </ProfileWrapper>
   );
 };
 
+function mapStateToProps(state) {
+  return {
+    email: state.authReducer.email,
+    nickname: state.authReducer.nickname
+  };
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   { uploadPic }
 )(Profile);
