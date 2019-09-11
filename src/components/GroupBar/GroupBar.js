a2fab20cd3a076e905fcd96b9d6a8a2bc9455e28
John Jeong <jjohnvi@Johns-MacBook-Pro.local>
2019-09-11 09:04:20 -0500

    minor fixes

--- src/components/GroupBar/GroupBar.js
+++ src/components/GroupBar/GroupBar.js
@@ -547,6 +547,9 @@ const AddUserCont = styled.div`
   border: 1px solid lightgray;
   box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.1),
     0 2px 4px 0 rgba(14, 30, 37, 0.12);
+  border-right: solid 1px lightgray;
+  border-top: 0;
+  border-bottom: 0;
 `;
 
 const AddUser = styled.button`
