12963718b49fd44584bb4bda70b56691604e20b6
hn4729 <henry0118@utexas.edu>
2019-09-11 11:56:09 -0500

    videos and images

--- src/components/GroupChatBox/Messages.js
+++ src/components/GroupChatBox/Messages.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
 import moment from "moment";
+import ReactPlayer from "react-player";
 import {
   FaEllipsisV,
   FaRegWindowClose,
@@ -76,6 +77,16 @@ const Messages = props => {
           </div>
         ) : props.dm.gif_url ? (
           <img src={props.dm.gif_url} alt="gif " />
+        ) : props.dm.img_url ? (
+          <ChatImage src={props.dm.img_url} alt="gif" className="image" />
+        ) : props.dm.video_url ? (
+          <ReactPlayer
+            controls
+            playing={false}
+            url={props.dm.video_url}
+            width="auto"
+            height="400px"
+          />
         ) : (
           <p className="messageText">{props.dm.message} </p>
         )}
@@ -127,6 +138,11 @@ export default connect(
     deleteGroupMessage
   }
 )(Messages);
+
+const ChatImage = styled.img`
+  height: 400px;
+  width: auto;
+`;
 
 export const ChatMessagesCont = styled.div`
   width: 100%;
