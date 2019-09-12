af24413798b94b631a041c0e44f4f68a6d5b8b8e
John Jeong <jjohnvi@Johns-MacBook-Pro.local>
2019-09-12 16:27:26 -0500

    last styling for gif form before presentation

--- src/components/GroupChatBox/GroupChatBox.js
+++ src/components/GroupChatBox/GroupChatBox.js
@@ -259,14 +259,23 @@ export const GIFForm = styled.form`
   margin-top: 15px;
 
   input {
-    padding: (3px 5px);
-    margin-right: 1rem;
-    border-radius: 5px;
+    width: 85%;
+    padding: 1rem;
+    border: none;
+    border-bottom: solid 2px ${props => props.theme.teal2};
+    box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.1),
+      0 2px 4px 0 rgba(14, 30, 37, 0.12);
+    outline: none;
   }
 
   button {
-    border-radius: 5px;
+    width: 15%;
+    padding: 1rem;
+    border: none;
+    border-bottom: solid 2px ${props => props.theme.teal1};
     background-color: ${props => props.theme.teal1};
+    box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.1),
+      0 2px 4px 0 rgba(14, 30, 37, 0.12);
   }
 `;
 
